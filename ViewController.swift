a7944b9e881072bab8d42028bdc6d63337c7dffc
lu4jay <jay@sysh.tc.edu.tw>
2019-08-20 14:06:59 +0800

    add

---
+++ ViewController.swift
@@ -0,0 +1,37 @@
+//
+//  ViewController.swift
+//  Class03
+//
+//  Created by RTC32 on 2019/8/20.
+//  Copyright © 2019 GIS. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+        let a = "Hi, Swift."
+        let  b = "世界好."
+        let c = 123
+        print(a)
+        print(b)
+        print(c)
+        
+        var 到超市 = true
+        var 看到西瓜 = true
+        
+        print( 包子的數量 )
+        print( 西瓜的數量 )
+        
+        func add (first : int, second : int) -> int {
+                    return 0
+        }
+        
+    }
+
+
+}
+
